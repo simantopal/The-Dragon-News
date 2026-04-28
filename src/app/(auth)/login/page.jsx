a830b2f085c11961225767fa3080c32da5f70763
simantopal <simantop13@gmail.com>
2026-04-29 00:04:46 +0600

    error message add

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -25,6 +25,13 @@ const LoginPage = () => {
             callbackURL: "/",
         });
         console.log(res, error);
+
+        if(error){
+            alert(error.message)
+        }
+        if(res){
+            alert("Signin successful");
+        }
     };
 
     return (
